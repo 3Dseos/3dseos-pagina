328619fb339155ed8430a0278b29cef1c16a9ec1
Nico Aquino <aquino_nicolas@hotmail.com>
2023-12-08 19:34:33 -0300

    Hace que cargen las cartas a traves de la base de datos

--- src/biblioteca.js
+++ src/biblioteca.js
@@ -1,53 +1,56 @@
+import { app } from './firebase'
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 
-(async () => {
-    try {
-        const response = await fetch("json/productos.json");
-        const data = await response.json();
+//Init services
+const db = getFirestore();
+//collection ref
+let collectionName = 'productos';
+const colRef = collection(db, collectionName);
 
-        // Obtiene el contenedor de productos
-        const productContainer = document.getElementById("product-container");
+// Obtiene el contenedor de productos
+const productContainer = document.getElementById("product-container");
 
-        // Itera sobre los datos del JSON y crea las tarjetas
-        data.forEach(function (producto) {
+getDocs(colRef)
+    .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            const producto = doc.data();
+
             // Obtén la primera imagen del array (puedes cambiar el índice según tus necesidades)
             const imagen = producto.imagenes[0];
 
             const cardHtml = `
-        <div class="card">
-            <div class="fixed-card">
-                <img src="${imagen}" class="img-container" alt="${producto.nombre}">
-                <div class="description">
-                    <span class="title">${producto.nombre}</span> 
-                    <button class="leer-btn" data-product="${producto.id}">Leer mas</button>
+                <div class="card">
+                    <div class="fixed-card">
+                        <img src="${imagen}" class="img-container" alt="${producto.nombre}">
+                        <div class="description">
+                            <span class="title">${producto.nombre}</span> 
+                            <button class="leer-btn" data-product="${producto.id}">Leer mas</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    `;
+            `;
 
             // Agrega la tarjeta al contenedor de productos
             productContainer.innerHTML += cardHtml;
-        });
 
+            // Obtén los elementos de los botones de leer más
+            const leerButtons = document.querySelectorAll('.leer-btn');
 
-        // Obtén los elementos de los botones de leer más
-        const leerButtons = document.querySelectorAll('.leer-btn');
+            // Maneja el clic en los botones de leer más
+            leerButtons.forEach(function (button) {
+                button.addEventListener('click', function (event) {
+                    // Obtén el identificador del producto desde el atributo data-product
+                    const productId = event.target.getAttribute('data-product');
 
-        // Maneja el clic en los botones de leer más
-        leerButtons.forEach(function (button) {
-            button.addEventListener('click', function (event) {
-                // Obtén el identificador del producto desde el atributo data-product
-                const productId = event.target.getAttribute('data-product');
-
-                // Redirige al usuario a la página de detalles del producto con el identificador del producto
-                window.location.href = `detallesProducto.html?id=${productId}`;
+                    // Redirige al usuario a la página de detalles del producto con el identificador del producto
+                    window.location.href = `detallesProducto.html?id=${productId}`;
+                });
             });
         });
-
-    } catch (err) {
-        console.log(`Error al cargar los datos del JSON: ${err.message}`);
-    }
-})();
-
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
 
 // funcioon barra de busqueda
 function buscarPorNombre() {
@@ -75,3 +78,46 @@ function quitarTildes(str) {
 } /* Source: https://es.stackoverflow.com/questions/62031/eliminar-signos-diacr%C3%ADticos-en-javascript-eliminar-tildes-acentos-ortogr%C3%A1ficos */
 
 document.getElementById("applyFiltersBtn").addEventListener("click", buscarPorNombre);
+
+
+//---------------------Subir JSON a la base de datos----------------------
+//Init services
+const jsonPath = "json/productos.json";
+
+async function leerYProcesarJson() {
+    try {
+        
+        const response = await fetch(jsonPath); 
+        const data = await response.json();
+        //------DEBUG--------
+        // console.log(data);
+        // for (const p of data) {
+        //   console.log(p);
+        // }
+        //-------------------
+        data.forEach(p => {
+            //delete p.id; //Quita el id del objeto para no subirlo a la db
+            console.log(p);
+            //addDoc(collection(db, collectionName), p)
+        });
+    } catch (error) {
+        console.log(`Se produjo un error: ${error.message}`);
+    }
+}
+
+const subirJsonBtn = document.querySelector('#SubirJson');
+subirJsonBtn.addEventListener('click', () => console.log("Sin efecto. Modificar el codigo para subirlo."));
+
+
+//getDocs(collection(db, collectionName))
+//    .then((snapshot) => {
+//        // console.log(snapshot);
+//        let arr = [];
+//        snapshot.docs.forEach((doc) => {
+//            arr.push({ ...doc.data(), id: doc.id })
+//        });
+//        console.log(arr);
+//    })
+//    .catch(err => {
+//        console.log(err.message);
+//    })
